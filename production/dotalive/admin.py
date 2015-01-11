7d0d814521beb8b8aa9256032ad0eccbfe03c9e3
Ubuntu <ubuntu@ip-172-31-24-55.us-west-2.compute.internal>
2015-01-11 03:05:30 +0000

    pack libs

--- production/dotalive/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from dotalive.models import stream_sites, streams
-# Register your models here.
-
-admin.site.register(stream_sites)
-admin.site.register(streams)
-
